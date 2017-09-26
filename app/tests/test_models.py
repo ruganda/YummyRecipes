8da8b7255804ff6378485db0b0d87081bc30e47a
ruganda <mubaruganda@gmail.com>
2017-09-26 06:00:08 +0300

    tested created user

---
+++ app/tests/test_models.py
@@ -0,0 +1,9 @@
+import unittest
+from app.models import User,Recipe_category,Recipe
+
+class UserTest(unittest.TestCase):
+    def setUp(self):
+        self.user = User("ruganda", "mubaruganda@gmail.com", "password")
+
+    def test_created_user(self):
+        self.assertIsInstance(self.user, User, 'User not created')
