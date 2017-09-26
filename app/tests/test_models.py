c2a10c9f633fcc11bc6cc0bdd7f765cb0abe5e82
ruganda <mubaruganda@gmail.com>
2017-09-26 06:13:30 +0300

    fixed indentation

--- app/tests/test_models.py
+++ app/tests/test_models.py
@@ -11,22 +11,22 @@ class UserTest(unittest.TestCase):
     def test_created_user(self):
         self.assertIsInstance(self.user, User, 'User not created')
     
-def test_add_recipe_category_category_added(self):
+    def test_add_recipe_category_category_added(self):
         self.assertEqual(self.user.add_recipe_category("dinner"),
                         "recipe category is added succesfully")
     
-def test_add_recipe_category_name_already_exists(self):
+    def test_add_recipe_category_name_already_exists(self):
         self.user.add_recipe_category("dinner")
         self.assertEqual(self.user.add_recipe_category
                          ("dinner"),
                          "recipe category already exists")
-def test_edit_recipe_category_not_found(self):
+    def test_edit_recipe_category_not_found(self):
         self.assertEqual(self.user.edit_recipe_category("absent", "newtype"),
                          "recipe_category not found")
- def test_edit_recipe_category_successful(self):
+    def test_edit_recipe_category_successful(self):
         self.user.add_recipe_category("Snacks")
         self.assertEqual(self.user.edit_recipe_category("Snacks","local foods"),"recipe_category not found")
 
 
 
-        
+
