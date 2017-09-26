4bb5b81f498d31ff4600899cd0b7971e1702f94c
ruganda <mubaruganda@gmail.com>
2017-09-26 09:18:25 +0300

    Added the home route

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template,g, flash, redirect, url_for, session, request, logging
-from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+#from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from models import User,Recipe_category,Recipe,repository
 
 USERS= []
@@ -7,6 +7,9 @@ recipe_categories =["food","ball","categories"]
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
