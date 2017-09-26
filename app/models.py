b737f31c2ee58468f192813d578892088372bc2d
ruganda <mubaruganda@gmail.com>
2017-09-26 06:44:35 +0300

    refactored to pass test

--- app/models.py
+++ app/models.py
@@ -6,8 +6,8 @@ class User:
         self.password =password
         self.recipe_categories =[]
 
-        def add_recipe_category(self,recipe_category):
-            if recipe_category not in self.recipe_categories:
+    def add_recipe_category(self,recipe_category):
+        if recipe_category not in self.recipe_categories:
             self.recipe_categories.append(recipe_category)
             return "recipe category is added succesfully"
         return "recipe category already exists"
