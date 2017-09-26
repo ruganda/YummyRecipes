47216bc656fdff48acfffb50782f249c1867b51b
ruganda <mubaruganda@gmail.com>
2017-09-26 23:38:57 +0300

    added forms

--- app/app.py
+++ app/app.py
@@ -7,6 +7,22 @@ recipe_categories =["dinner","supper","lunch"]
 
 app = Flask(__name__)
 
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])  
+    confirm = PasswordField('Confirm Password')
+
+class LoginForm(Form):
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),validators.EqualTo('confirm', message='Passwords do not match')
+    ])  
+
 @app.route('/')
 def home():
     return render_template('index.html')
